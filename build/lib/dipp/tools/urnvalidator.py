3f97286f99ec743c7e6c7b01bd3b4043a8a0faa8
Peter Reimer <reimer@hbz-nrw.de>
2011-05-23 14:51:21 +0000

    Preparing Release 0.0.2

--- build/lib/dipp/tools/urnvalidator.py
+++ build/lib/dipp/tools/urnvalidator.py
@@ -21,7 +21,7 @@ class URN:
 
     def get_dnb_status(self):
         """ connect to the resolver of the DNB an return the HTTP
-            status code and headers fpr a given URN
+            status code and headers for a given URN
         """
         
         dnb = httplib.HTTPConnection(RESOLVER, 80)
@@ -35,7 +35,7 @@ class URN:
 
     def is_registered(self):
         """a request for a registered URN is redirected to the registered
-           URL whith HTTP code 302. A not registered URN results in an error
+           URL with HTTP code 302. A not registered URN results in an error
            page with HTTP code 200.
         """
         
@@ -46,10 +46,17 @@ class URN:
 
     def is_valid(self):
         """A URN is only valid, when the actual URL of the document and the
-           registered URL are identical 
+           registered URL are identical. http and https are considered to be
+           the same url 
         """
         
-        if self.url == self.registered_url():
+        url = self.url.split('//')[-1] # chop of protocol
+        if self.is_registered():
+            reg_url = self.registered_url().split('//')[-1]
+        else:
+            reg_url = None
+            
+        if url == reg_url:
             return True
         else:
             return False
@@ -66,13 +73,14 @@ class URN:
 if __name__ == '__main__':
     
     URNs = (
-        ("urn:nbn:de:0009-11-29231","http://www.socwork.net/2011/1/salisbury"),
-        ("urn:nbn:de:0009-11-29231","http://www.socwork.net/2011/1/salisbury-wrong"),
-        ("urn:nbn:de:0009-11-fake","http://www.socwork.net/2011/1/salisbury"),
-        ("urn:nbn:de:0009-11-20391", "http://www.socwork.net/2009/1/special_issue/bailey")
+        ("urn:nbn:de:0009-11-29231", "http://www.socwork.net/2011/1/salisbury"),
+        ("urn:nbn:de:0009-11-29231", "http://www.socwork.net/2011/1/salisbury-wrong"),
+        ("urn:nbn:de:0009-11-fake",  "http://www.socwork.net/2011/1/salisbury"),
+        ("urn:nbn:de:0009-11-20391", "https://www.socwork.net/2009/1/special_issue/bailey")
     )
     
     for urn, url in URNs:
         x = URN(urn, url)
-        print x.urn, x.is_registered(), x.is_valid(), x.registered_url()
+        print x.urn, x.is_registered(), x.is_valid(), x.registered_url(), x.url
     
+        
