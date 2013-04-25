9fbce1f8d61b59fa14c404b0d57cb6ac8a6d0c48
Peter Reimer <reimer@hbz-nrw.de>
2013-04-25 13:56:44 +0000

    read most values from the xml and check urn/url for validity

--- dipp/tools/urnvalidator.py
+++ dipp/tools/urnvalidator.py
@@ -6,6 +6,7 @@
 __version__ = "$Id: urnvalidator 3066 2011-04-15 12:56:20Z reimer $"
 __author__ = "Peter Reimer <reimer@hbz-nrw.de>" 
 
+import argparse
 import httplib
 import urllib
 import urlparse
@@ -19,8 +20,7 @@ class URN:
         
         self.url = url
         self.urn = urn
-        self.status = False
-        self.headers = False
+        self.query_url = self.make_query_url()
 
 
     def make_query_url(self):
@@ -31,54 +31,60 @@ class URN:
         return query_url
 
     def get_dnb_response(self):
-        """ connect to the resolver of the DNB an return the HTTP
-            status code and headers for a given URN
+        """ connect to the resolver of the DNB an return the xml representation
+            of the resolved URN 
         """
         
         conn = httplib.HTTPConnection(RESOLVER, 80)
-        conn.request("GET", self.make_query_url())
+        conn.request("GET", self.query_url)
         response = conn.getresponse()
         xml = response.read()
         return xml
-
-    def is_registered(self):
-        xmldoc = xml.dom.minidom.parseString(self.get_dnb_response())
-        header = xmldoc.getElementsByTagName("pidef:header")[0]
-        status_code = header.getElementsByTagName("pidef:status_code")[0]
-        print status_code.firstChild.nodeValue
-        data = xmldoc.getElementsByTagName("pidef:data")[0]
-        url_infos = data.getElementsByTagName("pidef:url_info")
-        for url_info in url_infos:
-            url = url_info.getElementsByTagName("pidef:url")[0]
-            print url.firstChild.nodeValue
-
-    def is_valid(self):
-        """A URN is only valid, when the actual URL of the document and the
-           registered URL are identical. http and https are considered to be
-           the same url 
-        """
-        
-        url = self.url.split('//')[-1] # chop of protocol
-        if self.is_registered():
-            reg_url = self.registered_url().split('//')[-1]
-        else:
-            reg_url = None
-            
-        if url == reg_url:
-            return True
-        else:
-            return False
-        
-        
-    def registered_url(self):
-        """return the registered URL which is contained in the HTTP header
-           of the redirected request
-        """
-        h = dict(self.headers)
-        return h.get('location',None)
     
+    def parse_dnb_response(self):
+        
+        pidef = {"header":None, "data":None, "valid": False}
+        header = {}
+        data = []
+        xmldoc = xml.dom.minidom.parseString(self.get_dnb_response())
+        
+        # get header element
+        header_elem = xmldoc.getElementsByTagName("pidef:header")[0]
+        status_code_elem = header_elem.getElementsByTagName("pidef:status_code")[0]
+        header["status_code"] = status_code_elem.firstChild.nodeValue
+        pidef["header"] = header
+
+        # get data element
+        data_elem = xmldoc.getElementsByTagName("pidef:data")[0]
+        url_infos_elems = data_elem.getElementsByTagName("pidef:url_info")
+        urls = []
+        if len(url_infos_elems) > 0:
+            for url_infos_elem in url_infos_elems:
+                url_info = {}
+                for x in ('url','created','last_modified'):
+                    url_info[x] = getChildrenByName(url_infos_elem, ":".join(("pidef",x)))[0].firstChild.nodeValue
+                data.append(url_info)
+                urls.append(url_info["url"].split('//')[-1])
+            # check validity
+            if urls[0] == self.url.split('//')[-1]: 
+                pidef["valid"] = True
+        
+        pidef["data"] = data
+        return pidef
+
+def getChildrenByName(element, name):
+    children = []
+    for child in element.childNodes:
+        if child.nodeName == name:
+            children.append(child)
+    return children
 
 def main():
+
+    parser = argparse.ArgumentParser(description='check if the URN is registered with the DNB')
+    parser.add_argument("-l", "--url", action='store_true', help='the objects actual URL')
+    parser.add_argument("-n", "--urn", action='store_true', help='the objects URN')
+    args = parser.parse_args()
 
     URNs = (
         ("urn:nbn:de:0009-6-fake", "https://www.jvrb.org/past-issues/2.2005/248"),
@@ -88,8 +94,16 @@ def main():
    
     for urn, url in URNs:
         x = URN(urn, url)
-        x.is_registered()
-
+        answer = x.parse_dnb_response()
+        print "### %s ###" % urn
+        print "url: %s" % x.url
+        print "status_code: %s" % answer["header"]["status_code"] 
+        print "valid: %s" % answer["valid"] 
+        if len(answer["data"]) == 0:
+            print "no data found"
+        else:
+            for count, url_info in enumerate(answer["data"]):
+                print count, url_info['url']
 if __name__ == '__main__':
     main()
     
