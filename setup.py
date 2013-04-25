8c2c4bde933c58d379e34ecd21a180f9ab240313
Peter Reimer <reimer@hbz-nrw.de>
2013-04-25 14:17:52 +0000

    Preparing Release 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-__version__ = "0.1"
+__version__ = "0.2"
 __author__ = "Peter Reimer"
 
 setup(name='dipp.tools',
