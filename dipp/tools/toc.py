4df2195c8094e5384d0f272ae5501855f995f046
Peter Reimer <reimer@hbz-nrw.de>
2014-12-18 15:08:10 +0000

    output toc as ordered html list, nesting still missing

--- dipp/tools/toc.py
+++ dipp/tools/toc.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python
-from lxml.html import fromstring
+from BeautifulSoup import BeautifulSoup
+from lxml import etree
 
-f = open('article.html','r')
-html = f.read()
-f.close()
 
-tree = fromstring(html)
+class Toc:
 
-for a in tree.cssselect('h3 a'):
-    print 'found "%s" link to href "%s"' % (a.text, a.get('id'))
+    def __init__(self, html, levels=2):
+        
+        self.html = html
+        self.levels = levels
+        self.headings = [ "h%s" % i for i in range(1, self.levels + 1) ]
+
+    def get_toc(self):
+        soup = BeautifulSoup(self.html)
+        toc = []
+        for h in soup.findAll(self.headings):
+            level = int(h.name[1:])
+            anchor = h.find("a")
+            if anchor:
+                id = anchor.get('id',None)
+            else:
+                id = None
+            toc.append((level, id, h.text))
+
+        return toc
+    
+    def get_toc_html(self):
+        
+        toc = x.get_toc()
+        root = etree.Element("ol")
+        for level, id, text in toc:
+            item = etree.SubElement(root, "li" )
+            if id:
+                link = etree.SubElement(item, "a", href=id )
+                link.text = text
+            else:
+                item.text = text
+        return etree.tostring(root, pretty_print=True)
+
+
+if __name__ == '__main__':
+    f = open('article.html','r')
+    html = f.read()
+    f.close()
+    
+    x = Toc(html,levels = 4)
+    print x.get_toc_html()
