76c8d99f8ed4c8a6f4c52a502ea7b5af03071bea
Peter Reimer <reimer@hbz-nrw.de>
2018-09-21 07:52:14 +0200

    pep8

--- dipp/tools/toc.py
+++ dipp/tools/toc.py
@@ -5,24 +5,25 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class TOC:
 
     def __init__(self, html, levels=2):
-        
+
         self.html = html
         self.levels = levels
-        self.headings = [ "h%s" % i for i in range(1, self.levels + 1) ]
+        self.headings = ["h%s" % i for i in range(1, self.levels + 1)]
 
     def get_toc(self):
-        """parse the HTML version of an article and extract all headings up to 
+        """parse the HTML version of an article and extract all headings up to
            a given level and a return a list of tuples."""
 
         soup = BeautifulSoup(self.html)
         toc = []
 
-        # rip out first titlepage, since it contains headings 
+        # rip out first titlepage, since it contains headings
         # and article metadate not belonging to the toc
-        titlepage = soup.find("div", {"class":"titlepage"})
+        titlepage = soup.find("div", {"class": "titlepage"})
         if titlepage:
             titlepage.extract()
         else:
@@ -32,14 +33,13 @@ class TOC:
             level = int(heading.name[1:])
             anchor = heading.find("a")
             if anchor:
-                id = anchor.get('id',None)
+                id = anchor.get('id', None)
             else:
                 id = None
             toc.append((level, id, heading.text))
-        
+
         return toc
-    
-    
+
     def get_toc_html(self):
         """return the table of contents as an ordered list.
            TODO: make it a nested ordered list.
@@ -49,14 +49,14 @@ class TOC:
         logger.info('Max TOC level: %s ' % self.levels)
         root = etree.Element("ol")
         parent_level = 1
-        
+
         for level, id, text in toc:
             delta = level - parent_level
-            attrib = {"class":"toclevel%s" % (level - 1) }
-            item = etree.SubElement(root, "li", attrib=attrib )
+            attrib = {"class": "toclevel%s" % (level - 1)}
+            item = etree.SubElement(root, "li", attrib=attrib)
             if id:
                 anchor = "#" + id
-                link = etree.SubElement(item, "a", href=anchor )
+                link = etree.SubElement(item, "a", href=anchor)
                 link.text = text
             else:
                 item.text = text
@@ -65,10 +65,9 @@ class TOC:
 
 
 if __name__ == '__main__':
-    f = open('article.html','r')
+    f = open('article.html', 'r')
     html = f.read()
     f.close()
-    #html = """Kein gueltiges HTML"""    
-    toc = TOC(html,levels = 4)
+    # html = """Kein gueltiges HTML"""
+    toc = TOC(html, levels=4)
     print toc.get_toc_html()
-
